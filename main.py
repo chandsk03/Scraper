4fe94094220c3bd4ff7e5332ec0927403ba0bf62
Chandbasha <136245799+chandsk03@users.noreply.github.com>
2026-09-17 00:21:10 +0530

    Enable dynamic port configuration in main.py
    Updated the main.py file to allow dynamic port configuration from environment variables.

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def home():
     return{"API running"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
